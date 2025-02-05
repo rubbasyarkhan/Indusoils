e8e2be11c3bb0f790be8b02096a57a1c9418aad3
Rubbas yar khan <rubbasyarkhan007@gmail.com>
2025-02-05 22:10:33 +0500

    footer work

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,13 +4,27 @@ const Footer = () => {
   return (
     <>
     <footer className="bg-black text-white py-10 px-6">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 border-b border-gray-700 pb-6">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-6 border-b border-gray-700 pb-6">
         <div className="flex flex-col">
           <span className="text-xl font-bold flex items-center">
             <span className="text-red-600 text-3xl mr-2">■</span> INDUS
           </span>
         </div>
 
+        <div className="flex flex-col space-y-2">
+          <a href="#" className="hover:text-gray-400">Home</a>
+          <a href="#" className="hover:text-gray-400">About us</a>
+          <a href="#" className="hover:text-gray-400">Services</a>
+          <a href="#" className="hover:text-gray-400">Products</a>
+          <a href="#" className="hover:text-gray-400">Contact Us</a>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <a href="#" className="hover:text-gray-400">Home</a>
+          <a href="#" className="hover:text-gray-400">About us</a>
+          <a href="#" className="hover:text-gray-400">Services</a>
+          <a href="#" className="hover:text-gray-400">Products</a>
+          <a href="#" className="hover:text-gray-400">Contact Us</a>
+        </div>
         <div className="flex flex-col space-y-2">
           <a href="#" className="hover:text-gray-400">Home</a>
           <a href="#" className="hover:text-gray-400">About us</a>
